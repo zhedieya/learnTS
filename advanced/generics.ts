91f4684e1f7ae3de46b3d7177867ae2dfb2a2c8f
zhedieya <318992691@qq.com>
2022-10-05 15:31:34 +0800

    add some ts learning codes

--- advanced/generics.ts
+++ advanced/generics.ts
@@ -1,3 +1,77 @@
 // 泛型（Generics）是指在定义函数、接口或类的时候，不预先指定具体的类型，而在使用的时候再指定类型的一种特性。
+function createArray<T>(length: number, value: T): Array<T> {
+  let result: Array<T> = []
+  for (let i = 0; i < length; i++) {
+    result[i] = value
+  }
+  return result
+}
+console.log(createArray<string>(3, '1'))
 
+// 定义泛型的时候，可以一次定义多个类型参数：
 
+function swap<T, U>(tuple: [T, U]): [U, T] {
+  return [tuple[1], tuple[0]]
+}
+
+swap([7, 'seven']) // ['seven', 7]
+
+// 泛型约束
+interface hasLength {
+  length: number
+}
+
+function getLength<T extends hasLength>(arg: T): number {
+  console.log(arg.length)
+  return arg.length
+}
+
+getLength([1, 2])
+
+// 多个类型参数之间也可以互相约束：
+
+// T继承U，这样U中若有T中没有的属性，就会报错
+function copyFields<T extends U, U>(target: T, source: U): T {
+  for (let id in source) {
+    target[id] = (<T>source)[id]
+  }
+  return target
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 }
+
+copyFields(x, { b: 10, d: 20 })
+
+// 泛型接口
+interface CreateArrayFunc<T> {
+  (length: number, value: T): Array<T>
+}
+let createArray1: CreateArrayFunc<any>
+createArray1 = function <T>(length: number, value: T): Array<T> {
+  let result: Array<T> = []
+  for (let i = 0; i < length; i++) {
+    result[i] = value
+  }
+  return result
+}
+
+// 泛型类
+class GenericNumber<T> {
+  zeroValue: T
+  add: (x: T, y: T) => T
+}
+
+let myGenericNumber = new GenericNumber<number>()
+myGenericNumber.zeroValue = 0
+myGenericNumber.add = function (x, y) {
+  return x + y
+}
+
+// 泛型参数的默认类型
+function createArray2<T = string>(length: number, value: T): Array<T> {
+  let result: T[] = []
+  for (let i = 0; i < length; i++) {
+    result[i] = value
+  }
+  return result
+}
---
+++ basic/assertion.ts
@@ -0,0 +1,92 @@
+// 类型断言
+
+interface Cat {
+  name: string
+  run(): void
+}
+interface Fish {
+  name: string
+  swim(): void
+}
+
+let aaa :Fish = {
+  name: 'aaa',
+  swim() {
+    console.log('aaa swim')
+  }
+}
+console.log(typeof(aaa));
+
+function isFish(animal: Cat | Fish) {
+  // typeof animal.swim === 'function'  // 会报错
+  if (typeof (animal as Fish).swim === 'function') {
+    return true
+  }
+  return false
+}
+
+// 父类断言为子类
+class ApiError extends Error {
+  code: number = 0
+}
+class HttpError extends Error {
+  statusCode: number = 200
+}
+
+function isApiError(error: Error) {
+  if (typeof (error as ApiError).code === 'number') {
+    return true
+  }
+  return false
+}
+
+// 类型断言的限制
+interface Animal {
+  name: string
+}
+
+interface Dog {
+  name: string
+  run(): void
+}
+
+// 上述代码会被理解为继承.因为TypeScript 是结构类型系统，类型之间的对比只会比较它们最终的结构，而会忽略它们定义时的关系。
+interface Dog1 extends Animal {
+  run(): void
+}
+
+let bobby: Dog = {
+  name: 'bobby',
+  run() {
+    console.log('')
+  },
+}
+let ponny: Animal = bobby
+// 当 `Animal` 兼容 `Dog` 时，它们就可以互相进行类型断言了
+function testAnimal(animal: Animal) {
+  return (animal as Dog).run()
+}
+function testDog(dog: Dog) {
+  return (dog as Animal).name
+}
+
+// 将any断言为一个具体的类型
+let obj = {
+  a: 1,
+}
+function getCacheData(key: string): any {
+  return (obj as any).a
+}
+
+interface Cat {
+  name: string
+  run(): void
+}
+
+const tom = getCacheData('tom') as Cat
+// 在这个例子里  也可以用'类型声明'方式达成目的
+
+// 在这儿可以这么用的原因是any与Cat互相兼容，而若any换成Cat不兼容的类型，就会报错
+const tom1: Cat = getCacheData('tom')
+
+export {}
